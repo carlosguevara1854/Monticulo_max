b6477605b3b07a56ec513331ad19629d3b899b46
F.R.G <F.R.G@FRG>
2017-06-21 22:07:55 -0400

    Cambios en el orden del método eliminar.

--- Heap_max_str/src/heap_max_str/heap_max.java
+++ Heap_max_str/src/heap_max_str/heap_max.java
@@ -27,73 +27,12 @@ public class heap_max {
         root = insert_heap_max(root, key);
     }
 
-    public int erase_heap(int key) {
-        if (root.getP_left() == null && root.getP_rigth() == null) {
-            if (root.getKey_value() == key) {
-                root = null;
-                return 0;
-            }
-        } else if (root.getKey_value() == key) {
+    public void erase_heap(int key) {
+        if (root.getKey_value() == key) {
             root = erase_heap(root);
             check(root, null);
-            return 0;
         } else {
             erase_heap(root, key, false);
-            return 0;
-        }
-        return 1;
-    }
-
-    public void reader() {
-        try {
-            File f = new File("monticulo_in.txt");
-            if (f.exists()) {
-                FileReader fr = new FileReader(f);
-                BufferedReader br = new BufferedReader(fr);
-                String linea;
-                String[] keys_1, keys_2;
-                int i = 0;
-                while (((linea = br.readLine()) != null) && (i < 2)) {
-                    if (i == 0) {
-                        keys_1 = linea.split(" ");
-                        this.keys_add = new int[keys_1.length];
-                        for (int j = 0; j < keys_1.length; j++) {
-                            this.keys_add[j] = Integer.parseInt(keys_1[j]);
-                        }
-                    } else {
-                        keys_2 = linea.split(" ");
-                        this.keys_remove = new int[keys_2.length];
-                        for (int j = 0; j < keys_2.length; j++) {
-                            this.keys_remove[j] = Integer.parseInt(keys_2[j]);
-                        }
-                    }
-                    i++;
-                }
-            } else {
-            }
-        } catch (IOException e) {
-            System.err.println(e);
-            System.err.println("El archivo de entrada no existe.");
-        }
-    }
-
-    public void writer() {
-        try {
-            File f = new File("monticulo_out.txt");
-            FileWriter fw;
-            BufferedWriter bw;
-            if (f.exists()) {
-                fw = new FileWriter(f, true);
-                bw = new BufferedWriter(fw);
-                bw.newLine();
-            } else {
-                fw = new FileWriter(f);
-                bw = new BufferedWriter(fw);
-            }
-            bw.close();
-            fw.close();
-        } catch (IOException e) {
-            System.out.println(e);
         }
     }
 
@@ -235,5 +174,57 @@ public class heap_max {
         p_tree.setP_rigth(remove_max_node(p_tree.getP_rigth(), p_max_node));
         return p_tree;
     }
-}
 
+    public void reader() {
+        try {
+            File f = new File("monticulo_in.txt");
+            if (f.exists()) {
+                FileReader fr = new FileReader(f);
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+                String[] keys_1, keys_2;
+                int i = 0;
+                while (((linea = br.readLine()) != null) && (i < 2)) {
+                    if (i == 0) {
+                        keys_1 = linea.split(" ");
+                        this.keys_add = new int[keys_1.length];
+                        for (int j = 0; j < keys_1.length; j++) {
+                            this.keys_add[j] = Integer.parseInt(keys_1[j]);
+                        }
+                    } else {
+                        keys_2 = linea.split(" ");
+                        this.keys_remove = new int[keys_2.length];
+                        for (int j = 0; j < keys_2.length; j++) {
+                            this.keys_remove[j] = Integer.parseInt(keys_2[j]);
+                        }
+                    }
+                    i++;
+                }
+            } else {
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+            System.err.println("El archivo de entrada no existe.");
+        }
+    }
+
+    public void writer() {
+        try {
+            File f = new File("monticulo_out.txt");
+            FileWriter fw;
+            BufferedWriter bw;
+            if (f.exists()) {
+                fw = new FileWriter(f, true);
+                bw = new BufferedWriter(fw);
+                bw.newLine();
+            } else {
+                fw = new FileWriter(f);
+                bw = new BufferedWriter(fw);
+            }
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
